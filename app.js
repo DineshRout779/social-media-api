7cd9a291dd7a6a9fed38f1f791c63d78c0e58f50
Dinesh Rout <rddinesh779@gmail.com>
2022-01-14 15:13:25 +0530

    installed dependencies and connection to db

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log('db is connected'))
+  .catch((err) => console.log(err));
+
+// routes setup
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
