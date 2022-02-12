b7468a2d2cd94b131b4b88cd74ecb8734d8379d9
Dinesh Rout <rddinesh779@gmail.com>
2022-02-12 22:47:19 +0530

    refactoring

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema(
   {
-    userId: {
+    postedBy: {
       type: mongoose.Schema.ObjectId,
       ref: 'User',
       required: true,
