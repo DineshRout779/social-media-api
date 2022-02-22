ae834c01cbdd283dccc738cf19adb9d01cc52bf8
Dinesh Rout <rddinesh779@gmail.com>
2022-02-22 20:11:01 +0530

    set limit on file upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const postRoute = require('./routes/posts');
 const app = express();
 
 // middlewares
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(morgan('common'));
